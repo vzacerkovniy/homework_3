3c8d30aa4a947c239644488c11151adbc40ed059
v.zacerkovniy <vlad_26reg@mail.com>
2024-08-14 21:51:27 +0300

    add logging

--- src/external_api.py
+++ src/external_api.py
@@ -1,12 +1,25 @@
+import logging
+
 import requests
+
+logger = logging.getLogger("external_api")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler("../logs/external_api.log")
+file_formater = logging.Formatter("%(asctime)s - %(name)s -%(levelname)s: %(message)s")
+file_handler.setFormatter(file_formater)
+logger.addHandler(file_handler)
 
 
 def sum_to_convert(oper_dict: dict, api_key: str) -> float:
     """Функция по выводу сумм транзакций в рублях"""
     amount = oper_dict["operationAmount"]["amount"]
-    response = requests.get(
-        f"https://api.apilayer.com/exchangerates_data/live?base=RUB&symbols=EUR,USD&amount={amount} \
-                                                                          &apikey: {api_key}"
-    )
+    try:
+        response = requests.get(
+            f"https://api.apilayer.com/exchangerates_data/live?base=RUB&symbols=EUR,USD&amount={amount} \
+                                                                              &apikey: {api_key}"
+        )
 
-    return float(response.json()["result"])
+        return float(response.json()["result"])
+    except ValueError as ex:
+        logger.error(f"Произошла ошибка: {ex}")
+        return 0
