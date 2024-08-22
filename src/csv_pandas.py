b916ba4e02e2727962b320c3a57fc588e9ef8a0d
v.zacerkovniy <vlad_26reg@mail.com>
2024-08-22 11:19:32 +0300

    fix by flake8, isort, black

--- src/csv_pandas.py
+++ src/csv_pandas.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 
-def get_from_csv(path: str, delimiter: str = ';') -> list:
-    """ Функция принимает путь до csv-файла и возвращает список словарей с данными"""
+def get_from_csv(path: str, delimiter: str = ";") -> list:
+    """Функция принимает путь до csv-файла и возвращает список словарей с данными"""
     df = pd.read_csv(path, delimiter=delimiter).to_dict(orient="records")
     return df
 
@@ -11,7 +11,7 @@ def get_from_csv(path: str, delimiter: str = ';') -> list:
 
 
 def get_from_excel(path: str) -> list:
-    """ Функция принимает путь до excel-файла и возвращает список словарей с данными"""
+    """Функция принимает путь до excel-файла и возвращает список словарей с данными"""
     df = pd.read_excel(path).to_dict(orient="records")
     return df
 
